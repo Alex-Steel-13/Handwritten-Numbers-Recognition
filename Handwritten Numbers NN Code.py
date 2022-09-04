9ce8211a97564b7265249bc2af10e4d3dfada251
Alex Steel <99911854+Alex-Steel-13@users.noreply.github.com>
2022-09-04 10:31:49 +0100

    Update Handwritten Numbers NN Code.py

--- Handwritten Numbers NN Code.py
+++ Handwritten Numbers NN Code.py
@@ -67,16 +67,16 @@ def photo_editor(image):
     image /=255.0
     return image
 
-image_input_path = ["./input-copy/Predictions/Number 1.jpg",
-                    "./input-copy/Predictions/Number 2.jpg",
-                    "./input-copy/Predictions/Number 3.jpg",
-                    "./input-copy/Predictions/Number 4.jpg",
-                    "./input-copy/Predictions/Number 5.jpg",
-                    "./input-copy/Predictions/Number 6.jpg",
-                    "./input-copy/Predictions/Number 7.jpg",
-                    "./input-copy/Predictions/Number 8.jpg",
-                    "./input-copy/Predictions/Number 9.jpg",
-                    "./input-copy/Predictions/Number 16.jpg",
+image_input_path = ["./input - Copy/Predictions/Number 1.jpg",
+                    "./input - Copy/Predictions/Number 2.jpg",
+                    "./input - Copy/Predictions/Number 3.jpg",
+                    "./input - Copy/Predictions/Number 4.jpg",
+                    "./input - Copy/Predictions/Number 5.jpg",
+                    "./input - Copy/Predictions/Number 6.jpg",
+                    "./input - Copy/Predictions/Number 7.jpg",
+                    "./input - Copy/Predictions/Number 8.jpg",
+                    "./input - Copy/Predictions/Number 9.jpg",
+                    "./input - Copy/Predictions/Number 16.jpg",
                     ]
 
 fig,axs = plt.subplots(5,5,figsize=[24,21])
